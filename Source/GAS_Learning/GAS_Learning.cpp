3517a45ca031795cf7cfa607747d5237bc90519e
Anonymous_7wwr <1773414895@qq.com>
2024-12-07 14:34:10 +0800

    Initial Project Files

---
+++ Source/GAS_Learning/GAS_Learning.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GAS_Learning.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAS_Learning, "GAS_Learning" );
