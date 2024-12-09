6afb9dedfb4cc6bc038aae69e42336fc07c11a73
Anonymous_7wwr <1773414895@qq.com>
2024-12-09 10:16:33 +0800

    PlayerAndEnemyCharacterBase

---
+++ Source/GAS_Learning/Public/Character/Child/Aura.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "Aura.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_LEARNING_API AAura : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
